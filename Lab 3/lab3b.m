15bcf37e945a3f50dc47c2d1da49ddab19845afc
Chirantan Ganguly <chirantanganguly01@gmail.com>
2022-03-23 09:56:14 +0530

    Add files via upload

---
+++ Lab 3/lab3b.m
@@ -0,0 +1,35 @@
+% LAB 3 B
+% Chirantan Ganguly (420)
+% Amitrakshar Sanyal (422)
+% ================================
+n=0:100;
+f1=0.2
+f2=0.02
+x1=cos(f1*pi*n);
+x2=cos(f2*pi*n);
+y=x1.*x2;
+new_f1=f1+f2;
+new_f2=f1-f2;
+norm_f_beat=abs(f1-f2);
+figure(1);
+subplot(3,1,1);
+plot(n,x1);
+grid;
+xlabel('t');
+ylabel('Amplitude');
+legend('x_1(t)');
+title('Carrier Frequency x_1(t)');
+subplot(3,1,2);
+plot(n,x2);
+grid;
+xlabel('t');
+ylabel('Amplitude');
+legend('x_2(t)');
+title('Modulating Frequency x_2(t)');
+subplot(3,1,3);
+plot(n,y);
+grid;
+xlabel('t');
+ylabel('Amplitude');
+legend('y(t)');
+title('y(t)=x_1(t)*x_2(t)');
